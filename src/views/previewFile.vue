89e9af1bb5598a7199c5608a8d6435f9aa286913
吴蓉 <981723351@qq.com>
2019-04-10 14:27:25 +0800

    预览文件

--- src/views/previewFile.vue
+++ src/views/previewFile.vue
@@ -1,6 +1,19 @@
 <template>
  <section>
-
+   <div v-if="type==1">
+     <el-row v-for="(item,index) in imgList">
+       <img :src="item" />
+     </el-row>
+   </div>
+   <div v-if="type==2">
+     <table>
+       <tr v-for="(item,index) in excelList">
+         <template v-for="(items,indexs) in item">
+           <td>{{items}}</td>
+         </template>
+       </tr>
+     </table>
+   </div>
  </section>
 </template>
 
@@ -11,7 +24,9 @@
         name: "preview-file",
       data() {
         return {
-
+          type:undefined,
+          imgList:[],
+          excelList:[]
         }
       },
       methods: {
@@ -24,5 +39,8 @@
 </script>
 
 <style scoped>
-
+  th,td
+  {
+    border:1px solid black;
+  }
 </style>
